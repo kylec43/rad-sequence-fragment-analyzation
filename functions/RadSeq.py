c3fca59899a353df2b93e1d1686578fd3b0f807d
kylec43 <56184985+kylec43@users.noreply.github.com>
2021-09-15 05:22:03 -0500

    commit

--- functions/RadSeq.py
+++ functions/RadSeq.py
@@ -1,10 +1,26 @@
 import sys
-#Get Restriction Enzyme and Genome Data
+
+#This function will be responsible for fragmenting the genome
+def SliceGenome(genome, enzyme, probability):
+    return
 
 
-#Perform slicing
+
+#Get Restriction Enzyme, Genome, and Slice Probability Data
+genome = sys.argv[1]
+enzyme = sys.argv[2]
+probability = sys.arv[3]
+
+#Perform slicing, Get Fragments
+fragments = SliceGenome(genome, enzyme, probability)
 
 
 #Return results
-results = [sys.argv[1], sys.argv[2], sys.argv[3]]
+
+
+
+
+
+#This is for testing purposes. print() will send the results back to the server
+results = [genome, enzyme, probability]
 print(f"Python script response: {results}")
