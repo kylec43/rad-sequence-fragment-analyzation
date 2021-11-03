2a2a1c692ea1826d37e89368803aec62fcf21a5b
kylec43 <56184985+kylec43@users.noreply.github.com>
2021-11-02 23:38:11 -0500

    moved frontend javascript into firebase_controller. Added card to inform the user to sign up to upload restriction enzymes

--- public/js/firebase_controller.js
+++ public/js/firebase_controller.js
@@ -30,4 +30,24 @@ window.FirebaseController = {
             await setDoc(docRef, {'restriction_enzymes': [{'name': name, 'restrictionSite': restrictionSite, 'id': `${Date.now()}${name}${restrictionSite}`}]});
         }
     },
+
+    updateRestrictionEnzyme: async function (){
+
+        try{
+    
+            console.log("updating firestore");
+            var docRef = doc(getFirestore(), 'restriction_enzymes', DOC_NAME)
+            
+            restriction_enzymes[enzymeSelect.selectedIndex].name = enzymeName.value
+            restriction_enzymes[enzymeSelect.selectedIndex].restrictionSite = enzymeSite.value
+    
+            await setDoc(docRef, {restriction_enzymes});
+            document.getElementById(restriction_enzymes[enzymeSelect.selectedIndex]['id']).innerHTML = enzymeName.value;
+    
+            console.log("file updated")
+            
+        } catch(e){
+            console.log(`update failed: ${e}`);
+        }
+    }
 }
